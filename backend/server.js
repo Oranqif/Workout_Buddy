2d3448cd891fff5512096ed988b053ad874041cc
Thaqif Mikail <oranqif35@gmail.com>
2025-01-21 14:46:42 +0800

    Updated

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 // Require routes
 const workoutRoutes = require("./routes/workouts");
+// Require cors
+const cors = require("cors");
 // Set up the express app
 const app = express();
 
 // Middleware:
 // any code that executes between us getting a request on the server
 // and us sending a response back to the client
+app.use(cors());
 
 // Parse and attach data sent to server to request object
 app.use(express.json())
