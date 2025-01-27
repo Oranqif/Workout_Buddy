3d4f63719d877841789e93332aa9279b552c5f94
Thaqif Mikail <oranqif35@gmail.com>
2025-01-27 10:19:23 +0800

    Updated

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 // Require routes
 const workoutRoutes = require("./routes/workouts");
+const userRoutes = require("./routes/user");
+
 // Require cors
 // Set up the express app
 const app = express();
@@ -34,6 +36,7 @@ app.use((req, res, next) => {
 // Routes
 // workoutRoutes is triggered when we make a request to /api/workouts
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/user", userRoutes);
 
 // Connect to DB
 mongoose
