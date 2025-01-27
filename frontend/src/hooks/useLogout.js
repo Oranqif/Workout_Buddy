61c5fdd75352f04de0518f47ba7917770aa91961
Thaqif Mikail <oranqif35@gmail.com>
2025-01-27 15:34:04 +0800

    Updated

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,15 +1,15 @@
 import { useAuthContext } from "./useAuthContext"
 
 export const useLogout = () => {
-    const { dispatch } = useAuthContext
+    const { dispatch } = useAuthContext()
 
     const logout = () => {
         // Remove user from storage
         localStorage.removeItem('user')
 
         // Dispatch logout action
-        dispatch({type: 'LOGOUT'})
+        dispatch({ type: 'LOGOUT' })
     }
 
-    return {logout}
+    return { logout }
 }
