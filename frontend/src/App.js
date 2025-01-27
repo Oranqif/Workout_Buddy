bc030ac9b9b547a8c2e29645ebc7995ffb8d8e8c
Thaqif Mikail <oranqif35@gmail.com>
2025-01-27 11:49:48 +0800

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import Signup from './pages/Signup.js';
 import Navbar from './components/Navbar.js'
 
 function App() {
@@ -13,6 +15,14 @@ function App() {
             <Route
               path="/"
               element={<Home />}
+            />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/signup"
+              element={<Signup />}
             />
           </Routes>
         </div>
